8f875ab27e203cd96ce03283dc080965b3ae3bf0
henry <quocdat.381872@gmail.com>
2023-05-30 05:15:56 +0700

    chore: update config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,24 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
+import dns from 'dns';
+
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 
+dns.setDefaultResultOrder('verbatim'); // show url as localhost:{port}
+
 export default defineConfig({
-  cacheDir: './node_modules/.vite/my-book',
+  plugins: [react(), viteTsConfigPaths({ root: './' })],
 
-  server: {
-    port: 4200,
-    host: 'localhost',
-  },
+  cacheDir: './node_modules/.vite/my-book-fe',
 
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
-
-  plugins: [
-    react(),
-    viteTsConfigPaths({
-      root: './',
-    }),
-  ],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [
-  //    viteTsConfigPaths({
-  //      root: './',
-  //    }),
-  //  ],
-  // },
-
+  server: { port: 3200 },
+  preview: { port: 3300 },
   test: {
     globals: true,
-    cache: {
-      dir: './node_modules/.vitest',
-    },
+    cache: { dir: './node_modules/.vitest' },
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
 });
+
