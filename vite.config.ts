d49127f5894b8d2713887a9e33126c74e6cee8e6
henry <quocdat.381872@gmail.com>
2023-06-02 02:09:39 +0700

    feat: :twisted_rightwards_arrows: resolve conflicts from rebase

--- vite.config.ts
+++ vite.config.ts
@@ -2,32 +2,36 @@
 import dns from 'dns';
 
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 dns.setDefaultResultOrder('verbatim'); // show url as localhost:{port}
 
-export default defineConfig(({ command }) => ({
-  plugins: [react(), viteTsConfigPaths({ root: './' })],
+export default defineConfig(({ mode, command }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  const PORT = Number(process.env.VITE_PORT);
 
-  css: {
-    modules: {
-      localsConvention: 'camelCaseOnly',
-      generateScopedName:
-        command === 'serve' ? '[name]_[local]' : '[hash:base64:8]',
+  return {
+    plugins: [react(), viteTsConfigPaths({ root: './' })],
+
+    server: { port: PORT },
+    preview: { port: PORT + 1000 },
+
+    css: {
+      modules: {
+        localsConvention: 'camelCaseOnly',
+        generateScopedName:
+          command === 'serve' ? '[name]_[local]' : '[hash:base64:8]',
+      },
     },
-  },
 
-  cacheDir: './node_modules/.vite/my-book-fe',
+    cacheDir: './node_modules/.vite/my-book-fe',
 
-  server: { port: Number(process.env.PORT) },
-  define: {
-    'process.env': process.env,
-  },
-  test: {
-    globals: true,
-    cache: { dir: './node_modules/.vitest' },
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-  },
-}));
+    test: {
+      globals: true,
+      cache: { dir: './node_modules/.vitest' },
+      environment: 'jsdom',
+      include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    },
+  };
+});
